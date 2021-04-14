56a003088d1af969e172051f85ce9b8226a27948
cleogithub <survleo@hotmail.fr>
2021-04-14 11:18:29 +0200

    inversion lignes/colonnes, ajout de test, et type check

--- src/main/java/Dataframe.java
+++ src/main/java/Dataframe.java
@@ -4,24 +4,71 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dataframe {
-	private ArrayList<ArrayList<String>> data;
+	private ArrayList<ArrayList> data;
 	private int maxElementLength = 0;
+	
 
-	public Dataframe(ArrayList<ArrayList<String>> data) {
+	public Dataframe(ArrayList<ArrayList> data) throws Exception {
 		setData(data);
 	}
 	
-	public Dataframe(String csv) {
-		ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+//	public Dataframe(String csv) 
+//	{
+//		ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+//		try {
+//			Scanner fileScanner = new Scanner(new File(csv));
+//			while(fileScanner.hasNextLine()) {
+//				ArrayList<String> line = new ArrayList<String>();
+//				String lineString = fileScanner.nextLine();
+//				Scanner rowScanner = new Scanner(lineString);
+//				rowScanner.useDelimiter("");
+//				Boolean escape = false;
+//				String el = "";
+//				while(rowScanner.hasNext()) {
+//					String c = rowScanner.next();
+//					if(c.charAt(0) == '"'){ // Escaping
+//						if(escape) // Add the escaped " char to the element
+//							el += c;
+//						escape = !escape;
+//					} else if(!escape && c.charAt(0) == ',') { // End of element
+//						if(el.length() > this.maxElementLength)	// Used for pretty printing
+//							this.maxElementLength = el.length() + 1;
+//						if(el.length() > 0 && el.charAt(el.length()-1) == '"') // Removes last " char of the element
+//							el = el.substring(0, el.length() - 1);
+//						line.add(el);
+//						el = "";
+//					} else { // Element
+//						el += c;
+//					}
+//				}
+//				line.add(el); // Last element of the line (last column)
+//				lines.add(line);
+//				rowScanner.close();
+//			}
+//			fileScanner.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		try {
+//			setData(lines);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public Dataframe(String csv) 
+	{
+		ArrayList<ArrayList> columns = new ArrayList<ArrayList>();
 		try {
 			Scanner fileScanner = new Scanner(new File(csv));
 			while(fileScanner.hasNextLine()) {
-				ArrayList<String> line = new ArrayList<String>();
+				int columnIdx = 0;
 				String lineString = fileScanner.nextLine();
 				Scanner rowScanner = new Scanner(lineString);
 				rowScanner.useDelimiter("");
 				Boolean escape = false;
 				String el = "";
+				int rowIdx = 0;
 				while(rowScanner.hasNext()) {
 					String c = rowScanner.next();
 					if(c.charAt(0) == '"'){ // Escaping
@@ -31,37 +78,141 @@ public class Dataframe {
 					} else if(!escape && c.charAt(0) == ',') { // End of element
 						if(el.length() > this.maxElementLength)	// Used for pretty printing
 							this.maxElementLength = el.length() + 1;
+						
 						if(el.length() > 0 && el.charAt(el.length()-1) == '"') // Removes last " char of the element
-							el = el.substring(0, el.length() - 1);
-						line.add(el);
+							el = el.substring(0, el.length() - 1);		
+												
+						addElementAndCreateRowIfNecessary( columns, el, columnIdx);
+						
+						columnIdx++;						
 						el = "";
 					} else { // Element
 						el += c;
 					}
-				}
-				line.add(el); // Last element of the line (last column)
-				lines.add(line);
+				}				
+				addElementAndCreateRowIfNecessary( columns, el, columnIdx);
+				rowIdx++;
 				rowScanner.close();
 			}
-			setData(lines);
 			fileScanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		this.data = columns;
 	}
 	
-	private ArrayList<ArrayList<String>> getData() {
+	//Method for constructor
+	public void addElementAndCreateRowIfNecessary(ArrayList<ArrayList> columns, String el, int columnIdx)
+	{
+		boolean foundType = false;
+		//Test if the column already exist
+		if(columnIdx < columns.size()) {
+			try {
+				int elToAdd = Integer.parseInt(el); //If is Integer
+				foundType = true;
+				columns.get(columnIdx).add(Integer.parseInt(el));
+			} catch(Exception e) {}//Nothing to do for the exception
+			
+
+			if(!foundType)
+				try {
+					float elToAdd = Float.parseFloat(el); //If is float
+					foundType = true;
+					columns.get(columnIdx).add(Float.parseFloat(el));
+				} catch(Exception e) {}//Nothing to do for the exception
+			
+			if(!foundType)						  //Otherwise we add it like that
+				columns.get(columnIdx).add(el);
+		} else {
+
+			try {
+				int elToAdd = Integer.parseInt(el); //If is Integer
+				foundType = true;
+				columns.add(new ArrayList<Integer>());
+				columns.get(columnIdx).add(Integer.parseInt(el));
+			} catch(Exception e) {}//Nothing to do for the exception
+			
+
+			if(!foundType)
+				try {
+					float elToAdd = Float.parseFloat(el); //If is float
+					foundType = true;
+					columns.add(new ArrayList<Float>());
+					columns.get(columnIdx).add(Float.parseFloat(el));
+				} catch(Exception e) {}//Nothing to do for the exception
+			
+			if(!foundType)						  //Otherwise we add it like that
+				columns.add(new ArrayList<String>());
+				columns.get(columnIdx).add(el);							
+		}
+	}
+	
+	
+	/**
+	 * Getters, Setters
+	 */
+	
+	//Getters
+	private ArrayList<ArrayList> getData() {
 		return data;
 	}
 	
-	private void setData(ArrayList<ArrayList<String>> data) {
+	public Object getCell(int i, int j) {
+		return this.data.get(i).get(j);
+	}
+	
+	public int getNbLines() {
+		return this.data.size();
+	}
+	
+	public int getNbColumns() {
+		return this.data.size() > 0 ? this.data.get(0).size() : 0;
+	}
+	
+	//Setters
+	private void setData(ArrayList<ArrayList> data) throws Exception 
+	{		
+		if(!Dataframe.isValidDataframe(data))
+			throw new Exception("Erreur dans la structure de données du dataframe");
+		
 		this.data = data;
 	}
 	
+	
+	/*
+	 * Validation Methods
+	 */
+	
+	public static boolean isValidDataframe(ArrayList<ArrayList> dataframe)
+	{
+		boolean isValide = true;
+		int j = 0;
+		while(isValide && j < dataframe.size()) {
+			ArrayList line = dataframe.get(j);
+			int i = 0;
+			String type = "";
+			if(line.size() > 0)
+				type = line.get(i).getClass().getName();
+			
+			while(isValide && i < line.size()) {
+				if(line.get(i).getClass().getName() != type)
+					isValide = false;
+				i++;
+			}
+			j++;
+		}
+		return isValide;
+	}
+	
+	
+	/*
+	 * toString Methods
+	 */
+	
 	private String lineToString(int i){
 		String s = "";
-		for(String element : data.get(i)) {
-			String elementTmp = element;
+		for(Object element : data.get(i)) {
+			String elementTmp = element.toString();
 			while(elementTmp.length() < this.maxElementLength)
 				elementTmp += " ";
 			s += elementTmp;
@@ -76,17 +227,5 @@ public class Dataframe {
 			s += lineToString(i);
 		}
 		return s;
-	}
-	
-	public String getCell(int i, int j) {
-		return this.data.get(i).get(j);
-	}
-	
-	public int getNbLines() {
-		return this.data.size();
-	}
-	
-	public int getNbColumns() {
-		return this.data.size() > 0 ? this.data.get(0).size() : 0;
 	}
 }
--- src/test/java/DataframeTest.java
+++ src/test/java/DataframeTest.java
@@ -1,8 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.junit.rules.ExpectedException;
 
 public class DataframeTest {
 	public Dataframe d;
@@ -25,10 +31,78 @@ public class DataframeTest {
 	@Test
 	public void testGetCell() {
 		assertEquals("John", d.getCell(0, 0));
-		assertEquals("Jack", d.getCell(1, 0));
-		assertEquals("8075", d.getCell(0, 5));
-		assertEquals("John \"Da Man, da\", \", da,\"", d.getCell(2, 0));
-		assertEquals("7452 Terrace \"At the Plaza\" road", d.getCell(3, 2));
+		assertEquals("Jack", d.getCell(0, 1));
+		assertEquals(8075, d.getCell(5, 0));
+		assertEquals("John \"Da Man, da\", \", da,\"", d.getCell(0, 2));
+		assertEquals("7452 Terrace \"At the Plaza\" road", d.getCell(2, 3));
 	}
-
+	
+	@Test
+	public void testWrongOneTypePerColonne()
+	{	
+		ArrayList<ArrayList> wrongDataframe = new ArrayList<ArrayList>();
+		ArrayList line = new ArrayList<Integer>();
+		line.add(0);
+		line.add(1);		
+		line.add(2);		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add(0);
+		line.add(3);		
+		line.add(4);		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add("test");
+		line.add(5.3);		
+		line.add("dssfdsf");		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add(5.3);
+		line.add(4.2);		
+		line.add(4.5);		
+		wrongDataframe.add(line);
+		
+		try {
+			new Dataframe(wrongDataframe);
+			fail();
+		} catch(Exception e) {}
+	}
+	
+	@Test
+	public void testGoodOneTypePerColonne()
+	{	
+		ArrayList<ArrayList> wrongDataframe = new ArrayList<ArrayList>();
+		ArrayList line = new ArrayList<Integer>();
+		line.add(0);
+		line.add(1);		
+		line.add(2);		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add(0);
+		line.add(3);		
+		line.add(4);		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add("test");
+		line.add("5.3");		
+		line.add("dssfdsf");		
+		wrongDataframe.add(line);
+		
+		line = new ArrayList();
+		line.add(5.3);
+		line.add(4.2);	
+		line.add(4.5);
+		wrongDataframe.add(line);
+		
+		try {
+			new Dataframe(wrongDataframe);
+		} catch(Exception e) {
+			fail();
+		}
+	}
 }
